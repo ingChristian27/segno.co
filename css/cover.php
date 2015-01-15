505c7e9ca48c77a3bdf5637a39bc5ed75d575629
christian duban dachiardi <cristianoliveros_27@hotmail.com>
2015-01-15 13:40:31 +0100

    Cover
    se realizan los primeros estilos en el cover, y algunas nuevas clases en
    la estructura

--- css/cover.php
+++ css/cover.php
@@ -1,3 +1,35 @@
 #cover {
-    background: <?php echo $colors['white'] ?>
+    background: <?php echo $colors['white'] ?>;
+    padding-top: 19px;
+	height: 600px;
+	background-image: url('images/background_header.jpg');
+                        -webkit-background-size: cover;
+                        -moz-background-size: cover;
+                        -o-background-size: cover;
+                        -ms-background-size: cover;
+                        background-size: cover;
+                        background-position: 50% 50%;
+                        -ms-behavior: url(/backgroundsize.min.htc);
+}
+#lang{
+	width: 220px;
+	height: 27px;
+	border-style: solid;
+	border-color: #1B3935;
+	text-transform: uppercase;
+	background-image: url("images/background_lang.png");
+	background-repeat: repeat;
+
+}
+#logo{
+	background-image: url("images/logo.fw.png");
+	width: 646px;
+	height: 140px;
+	display: block;
+}
+#slogan{
+	background-image: url("images/slogan.png");
+	height: 108px;
+	display: block;
+	background-position: center center;
 }
--- html/cover.php
+++ html/cover.php
@@ -1,14 +1,16 @@
 <div id="cover">
-    <div class="container">
-        <a id="logo"><img src="<?php echo $cover['logo'] ?>" alt=""></a>
-        <h1><?php echo $cover['text'] ?></h1>
-        <div id="lang">
-            <a href="#" class="">
+    <div class="">
+
+        <a id="logo"class="col-md-6"><img src="<?php echo $cover['logo'] ?>" alt=""></a>
+        <h1 id="slogan" class="col-md-6"><?php echo $cover['text'] ?></h1>
+            <div id="lang" class="pull-right">
+            <a href="#" class="col-md-6 text-center">
                 <?php echo $cover[ 'lang'][0] ?>
             </a>
-            <a href="#" class="">
+            <a href="#" class="col-md-6 text-center">
                 <?php echo $cover[ 'lang'][1] ?>
             </a>
         </div>
     </div>
 </div>
+
