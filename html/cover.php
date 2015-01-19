ff2d532ca10c9c85b26fc76bb3f9e833e5b78578
JP Juliao <jpjuliao@gmail.com>
2015-01-19 13:51:50 -0500

    Cover conflict

--- html/cover.php
+++ html/cover.php
@@ -1,23 +1,11 @@
 <div id="cover">
-<<<<<<< HEAD
+   
     <div class="">
 
         <a id="logo"class="col-md-6"><img src="<?php echo $cover['logo'] ?>" alt=""></a>
         <h1 id="slogan" class="col-md-6"><?php echo $cover['text'] ?></h1>
             <div id="lang" class="pull-right">
             <a href="#" class="col-md-6 text-center">
-=======
-   
-    <div class="container">
-       
-        <a id="logo"><img src="<?php echo $cover['logo'] ?>" alt=""></a>
-        
-        <h1><?php echo $cover['text'] ?></h1>
-        
-        <div id="lang">
-           
-            <a href="#" class="">
->>>>>>> origin/master
                 <?php echo $cover[ 'lang'][0] ?>
             </a>
             <a href="#" class="col-md-6 text-center">
@@ -27,10 +15,5 @@
         </div>
         
     </div>
-<<<<<<< HEAD
-</div>
-
-=======
     
 </div>
->>>>>>> origin/master
