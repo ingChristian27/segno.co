f9309294f2bb930598bd9cb75170917b666c2c03
JP Juliao <jpjuliao@gmail.com>
2015-01-19 12:21:20 -0500

    HTML services

--- html/services.php
+++ html/services.php
@@ -1,5 +1,25 @@
 <div id="services">
-   
-   
-    
+
+    <h2><?php echo $services['title'] ?></h2>
+
+    <div class="panel-group" id="servicios-accordion" role="tablist" aria-multiselectable="true">
+       
+        <div class="panel panel-default">
+           
+            <div class="panel-heading" role="tab" id="headingOne">
+               
+                <h4 class="panel-title">
+                   
+                    <a data-toggle="collapse" data-parent="#servicios-accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    </a>
+                    
+                </h4>
+                
+            </div>
+            
+        </div>
+        
+    </div>
+
+
 </div>
