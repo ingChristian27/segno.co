88c8198283de0c5fe0a6f23f0b42532dcad2c226
JP Juliao <jpjuliao@gmail.com>
2015-01-19 13:58:15 -0500

    cover fix

---
+++ html/cover.php
@@ -0,0 +1,23 @@
+<div id="cover">
+<<<<<<< HEAD
+   
+    <div class="">
+
+        <a id="logo"class="col-md-6"><img src="<?php echo $cover['logo'] ?>" alt=""></a>
+        <h1 id="slogan" class="col-md-6"><?php echo $cover['text'] ?></h1>
+            <div id="lang" class="pull-right">
+            <a href="#" class="col-md-6 text-center">
+                <?php echo $cover[ 'lang'][0] ?>
+            </a>
+            <a href="#" class="col-md-6 text-center">
+                <?php echo $cover[ 'lang'][1] ?>
+            </a>
+            
+        </div>
+        
+    </div>
+    
+</div>
+
+
+>>>>>>> origin/master
