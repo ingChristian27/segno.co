155409d94cde79e80900baa42ab5e6b995f29e3d
christian duban dachiardi <cristianoliveros_27@hotmail.com>
2015-01-21 11:19:20 +0100

    imagen about responsive

--- html/about.php
+++ html/about.php
@@ -4,8 +4,9 @@
         
         <h2><?php echo $about['title'] ?></h2>
         <p><?php echo $about['text'] ?></p>
-        <img src="<?php echo $about['img'] ?>" alt="">
+        <img src="<?php echo $about['img'] ?>" alt="" class="img-responsive">
         
     </div>
     
 </div>
+
